a36d8cc4ef1c400db021ca81a53a23d9972d61b9
rahul2103 <rahulsingh2103@yahoo.com>
2022-04-04 01:06:23 +0530

    implement pwa app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+5.times do |i|
+  Post.create(title: "Post #{i}", content: 'A lot of stuff')
+end
